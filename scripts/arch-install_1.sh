8ea7ed2874435f4ecff7c7b339c76c9d386953db
gargulaonline <fredcox@gmail.com>
2023-09-27 08:32:02 -0300

    ok

--- scripts/arch-install_1.sh
+++ scripts/arch-install_1.sh
@@ -2,6 +2,18 @@
 #
 DISK_DEVICE='nvme0n1'
 
+# Is a Virtual Machine? YES or NO ?
+IS_VIRTUAL_MACHINE="NO"
+
+if [ $IS_VIRTUAL_MACHINE == "YES" ]
+then
+	BOOT="$DISK_DEVICE"1
+	ROOT="$DISK_DEVICE"2
+else
+	BOOT="$DISK_DEVICE"p1
+	ROOT="$DISK_DEVICE"p2
+fi
+
 loadkeys br-abnt2
 timedatectl set-ntp true
 setfont ter-132b
@@ -14,23 +26,25 @@ sgdisk -n 1:0:+300M /dev/$DISK_DEVICE -t 1:ef00 -c efi
 sgdisk --largest-new=2 /dev/$DISK_DEVICE
 
 
-mkfs.fat -F32 /dev/"$DISK_DEVICE"p1
-mkfs.btrfs -f /dev/"$DISK_DEVICE"p2
-mount /dev/"$DISK_DEVICE"p2 /mnt
+mkfs.fat -F32 /dev/$BOOT
+mkfs.btrfs -f /dev/$ROOT
+mount /dev/"$ROOT" /mnt
 btrfs su cr /mnt/@
 btrfs su cr /mnt/@snapshots
 btrfs su cr /mnt/@var_log
 umount /mnt
-
-mount -o noatime,compress=lzo,space_cache=v2,subvol=@ /dev/"$DISK_DEVICE"p2 /mnt
+mount -o noatime,compress=lzo,space_cache=v2,subvol=@ /dev/"$ROOT" /mnt
 mkdir -p /mnt/{boot,var,.snapshots}
 mkdir -p /mnt/var/log
 mount -o noatime,compress=lzo,space_cache=v2,subvol=@var_log \
-	/dev/"$DISK_DEVICE"p2 /mnt/var/log
+	/dev/"$ROOT" /mnt/var/log
 mount -o noatime,compress=lzo,space_cache=v2,subvol=@snapshots \
-	/dev/"$DISK_DEVICE"p2 /mnt/.snapshots
+	/dev/"$ROOT" /mnt/.snapshots
 
-mount /dev/"$DISK_DEVICE"p1 /mnt/boot
+mount /dev/"$BOOT" /mnt/boot
+
+
+
 pacstrap /mnt base base-devel linux linux-firmware amd-ucode openssh vim git duf
 genfstab -U /mnt >> /mnt/etc/fstab
 
