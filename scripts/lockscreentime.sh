4ad7535f59e6100a5885c5696cbaba8addf7b086
gargulaonline <fredcox@gmail.com>
2023-09-28 13:48:27 -0300

    ok

--- scripts/lockscreentime.sh
+++ scripts/lockscreentime.sh
@@ -2,7 +2,7 @@
 
 if [ -f "/usr/bin/swayidle" ]; then
     echo "swayidle is installed."
-    swayidle -w timeout 300 'swaylock -f' timeout 360 'hyprctl dispatch dpms off' resume 'hyprctl dispatch dpms on'
+    swayidle -w timeout 300 'swaylock -f' timeout 360 'hyprctl dispatch dpms off'
 else
     echo "swayidle not installed."
 fi;
