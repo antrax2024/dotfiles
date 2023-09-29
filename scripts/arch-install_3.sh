d5ea3d12b75eea6ac642d04dc04d8a59cac4b08e
gargulaonline <fredcox@gmail.com>
2023-09-29 10:36:04 -0300

    ok

--- scripts/arch-install_3.sh
+++ scripts/arch-install_3.sh
@@ -52,6 +52,18 @@ evince
 gthumb
 	)
 
+
+printStatus "Install Chaotic AUR Repository"
+sudo pacman-key --recv-key 3056513887B78AEB --keyserver keyserver.ubuntu.com
+sudo pacman-key --lsign-key 3056513887B78AEB
+sudo pacman -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-keyring.pkg.tar.zst' 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-mirrorlist.pkg.tar.zst'
+
+echo '[chaotic-aur]' | sudo tee -a /etc/pacman.conf >/dev/null
+echo 'Include = /etc/pacman.d/chaotic-mirrorlist' | sudo tee -a /etc/pacman.conf >/dev/null
+
+paru
+
+
 AUR_PKGS=(
 dunst
 fuzzel
