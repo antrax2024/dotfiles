7fabe43d0f94de44954dde0c1c24f5e6405c2ff1
gargulaonline <fredcox@gmail.com>
2023-09-29 10:32:17 -0300

    k

--- scripts/install-chaotic-aur.sh
+++ scripts/install-chaotic-aur.sh
@@ -1,18 +1,16 @@
 #!/bin/bash
 #
 
-su
 
-pacman-key --recv-key 3056513887B78AEB --keyserver keyserver.ubuntu.com
+sudo pacman-key --recv-key 3056513887B78AEB --keyserver keyserver.ubuntu.com
 
-pacman-key --lsign-key 3056513887B78AEB
+sudo pacman-key --lsign-key 3056513887B78AEB
 
-pacman -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-keyring.pkg.tar.zst' 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-mirrorlist.pkg.tar.zst'
+sudo pacman -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-keyring.pkg.tar.zst' 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-mirrorlist.pkg.tar.zst'
 
-echo '[chaotic-aur]' >> /etc/pacman.conf
-echo 'Include = /etc/pacman.d/chaotic-mirrorlist' >> /etc/pacman.conf
 
-exit
+echo '[chaotic-aur]' | sudo tee -a /etc/pacman.conf >/dev/null
+echo 'Include = /etc/pacman.d/chaotic-mirrorlist' | sudo tee -a /etc/pacman.conf >/dev/null
 
 paru
 
