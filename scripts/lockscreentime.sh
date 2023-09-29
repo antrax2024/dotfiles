52cf05003b243c84e7fab64544c026dba62564aa
gargulaonline <fredcox@gmail.com>
2023-09-29 13:27:02 -0300

    ok

--- scripts/lockscreentime.sh
+++ scripts/lockscreentime.sh
@@ -1,9 +1,5 @@
 #/bin/sh
 
-if [ -f "/usr/bin/swayidle" ]; then
-    echo "swayidle is installed."
-    swayidle -w timeout 300 'swaylock -f' timeout 360
-else
-    echo "swayidle not installed."
-fi;
 
+
+swayidle -w timeout 300 'swaylock -f -c 000000'
