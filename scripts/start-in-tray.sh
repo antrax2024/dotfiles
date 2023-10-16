9e90984392fe2ec57b6d25478ea21f6c05c70b4a
gargulaonline <fredcox@gmail.com>
2023-10-16 09:25:08 -0300

    electron-flags.conf

--- scripts/start-in-tray.sh
+++ scripts/start-in-tray.sh
@@ -2,6 +2,13 @@
 # start waybar and programs with tray icons after pause
 
 sleep 20
+
+# Sound Applet
+pasystray &
+# Google Drive
 insync start &
+# Network Applet
 nm-applet --indicator &
+# Bluetooth Applet
 blueman-applet &
+
