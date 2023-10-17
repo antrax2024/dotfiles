6c3e05668b7c8e024cdb5c3aea3d591e2eee8925
gargulaonline <fredcox@gmail.com>
2023-10-16 21:17:10 -0300

    limine

--- config-nvim-lua-user/init.lua
+++ config-nvim-lua-user/init.lua
@@ -18,7 +18,7 @@ return {
   },
 
   -- Set colorscheme to use
-  colorscheme = "github_dark",
+  colorscheme = "onedark_dark",
 
   -- Diagnostics configuration (for vim.diagnostics.config({...})) when diagnostics are on
   diagnostics = {
