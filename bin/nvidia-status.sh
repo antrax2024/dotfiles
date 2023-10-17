860ae94fe92db9b209f1b5ee2d665931c89bf723
gargulaonline <fredcox@gmail.com>
2023-10-17 16:57:38 -0300

    ok

---
+++ bin/nvidia-status.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#export LC_NUMERIC="en_US.UTF-8"
+
+TEMP=$(nvidia-smi --query-gpu=temperature.gpu --format=csv,noheader)
+
+UTILIZATION=$(nvidia-smi --query-gpu=utilization.gpu --format=csv,noheader)
+
+
+#echo "$UTILIZATION $TEMP °C"
+
+printf "%s" $UTILIZATION
+printf " %2d°C\n" $TEMP
