490e6d4c2147d141faadb0cb4ac570203358e3c8
gargulaonline <fredcox@gmail.com>
2023-10-17 21:40:27 -0300

    code laggy

--- scripts/arch-install_2.sh
+++ scripts/arch-install_2.sh
@@ -1,3 +1,4 @@
+#!/usr/bin/bash
 # CONFIG VARS
 #
 HOSTNAME='gargulabox'
@@ -5,23 +6,18 @@ USERNAME='gargula'
 EDITOR='nvim'
 ROOT_PARTITION='nvme1n1p2'
 
-###############################
-# Functions
-function printStatus() {
-	echo "$1"
-	echo "..."
-	sleep 2
-}
-###############################
+source functions.sh
+source packages.sh
 
+printStatus "localtime, hardware clock, vconsole, etc....."
 ln -sf /usr/share/zoneinfo/America/Recife /etc/localtime
 hwclock --systohc
 echo 'en_US.UTF-8 UTF-8' >> /etc/locale.gen
 locale-gen
 echo 'LANG=en_US.UTF-8' >> /etc/locale.conf
 echo "KEYMAP=br-abnt2" >> /etc/vconsole.conf
-#echo "FONT=ter-132b" >> /etc/vconsole.conf
 
+printStatus "hostname, /etc/hosts ..."
 echo "$HOSTNAME" >> /etc/hostname
 echo -e "127.0.0.1\tlocalhost" >> /etc/hosts
 echo -e "::1\t\tlocalhost" >> /etc/hosts 
@@ -31,69 +27,6 @@ printStatus "Root password hummmmmmm..."
 passwd
 
 printStatus "Install pacman packages...."
-PACMAN_PKGS=(
-mpd
-wget
-rsync
-sshfs
-exa
-bat
-notepadqq
-ethtool
-evince
-cronie
-gthumb
-gimp
-tumbler
-yt-dlp
-lm_sensors
-w3m
-fish
-python-pip
-greetd
-grub-customizer
-xorg-xhost
-breeze
-cliphist
-xarchiver
-unzip
-system-config-printer
-usbutils
-avahi
-nss-mdns
-nvme-cli
-mako
-networkmanager
-network-manager-applet
-dialog
-mtools
-dosfstools
-snapper
-bluez
-bluez-utils
-xdg-utils
-xdg-user-dirs
-alsa-utils
-inetutils
-terminus-font
-nmap
-less
-blueman
-pipewire-jack
-pipewire-alsa
-pipewire-pulse
-limine
-speedcrunch
-discord
-telegram-desktop
-imagemagick
-ntp
-pavucontrol
-mlocate
-copyq
-python-psutil
-)
-
 pacman -S ${PACMAN_PKGS[*]}
 
 
@@ -113,7 +46,7 @@ useradd -mG wheel $USERNAME
 passwd $USERNAME
 visudo
 
-printStatus "Installing limine bootloader..."
+printStatus "Installing limine bootloader OHHHH YESSSS!..."
 mkdir -p /boot/EFI/BOOT
 cp /usr/share/limine/BOOTX64.EFI /boot/EFI/BOOT
 curl -fssl https://gargulaonline.github.io/limine-config/limine.cfg -o /boot/limine.cfg
