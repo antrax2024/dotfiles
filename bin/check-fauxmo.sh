30d53d4d713f93d73fb6d96b585297049362fe62
gargulaonline <fredcox@gmail.com>
2023-10-18 14:34:28 -0300

    fauxmo ok

--- bin/check-fauxmo.sh
+++ bin/check-fauxmo.sh
@@ -8,7 +8,7 @@ do
 		echo "Fauxmo already running"
 	else
 		echo "Fauxmo not running..."
-		/home/gargula/.local/bin/fauxmo -c /home/orangepi/fauxmo/config.json
+		$HOME/.local/bin/fauxmo -c $HOME/dotfiles/fauxmo/config.json
 		echo "Down..."
 	fi
 	sleep 5
