e0b06907aeaa4648563dff6d237ec7da099123a6
gargulaonline <fredcox@gmail.com>
2023-10-18 07:27:15 -0300

    ok

--- scripts/screensaver.sh
+++ scripts/screensaver.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/bash
 
-kitty --start-as=fullscreen /usr/bin/unimatrix -n -s 96 -l o
+#kitty --start-as=fullscreen /usr/bin/unimatrix -n -s 96 -l o
+footclient -F /usr/bin/unimatrix -n -s 96 -l o
