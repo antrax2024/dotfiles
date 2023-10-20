115dcd071b3ee11e7c849a5bdf976da4539c423a
gargulaonline <fredcox@gmail.com>
2023-10-20 11:14:12 -0300

    ok

--- bin/upgrade.sh
+++ bin/upgrade.sh
@@ -6,7 +6,13 @@
 echo "Update Arch Linux mirror List..."
 export TMPFILE="$(mktemp)" 
 sudo true 
-rate-mirrors --min-per-mirror=400 --save=$TMPFILE arch 
+rate-mirrors --protocol https \
+    --per-mirror-timeout 300 \
+    --top-mirrors-number-to-retest 0 \
+    --min-per-mirror=400 \
+    --allow-root --disable-comments \
+    --save=$TMPFILE arch
+
 sudo mv /etc/pacman.d/mirrorlist /etc/pacman.d/mirrorlist-backup 
 sudo mv $TMPFILE /etc/pacman.d/mirrorlist
 
@@ -14,7 +20,13 @@ sudo mv $TMPFILE /etc/pacman.d/mirrorlist
 echo "Update Chaotic-AUR mirrorlist"
 cat /dev/null>$TMPFILE
 
-rate-mirrors --min-per-mirror=400 --save=$TMPFILE chaotic-aur 
+rate-mirrors --protocol https \
+    --per-mirror-timeout 300 \
+    --top-mirrors-number-to-retest 0 \
+    --min-per-mirror=400 \
+    --allow-root --disable-comments \
+    --save=$TMPFILE chaotic-aur
+
 sudo mv /etc/pacman.d/chaotic-mirrorlist /etc/pacman.d/chaotic-mirrorlist-backup 
 sudo mv $TMPFILE /etc/pacman.d/chaotic-mirrorlist 
 
