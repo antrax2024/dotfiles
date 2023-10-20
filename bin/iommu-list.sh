82b858abc5df357abf69d196fd57408df190bbe2
gargulaonline <fredcox@gmail.com>
2023-10-19 21:57:10 -0300

    ok

--- bin/iommu-list.sh
+++ bin/iommu-list.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/bash
-
-for d in /sys/kernel/iommu_groups/*/devices/*; do
-  n=${d#*/iommu_groups/*}; n=${n%%/*}
-  printf 'IOMMU Group %s ' "$n"
-  lspci -nns "${d##*/}"
-done
+shopt -s nullglob
+for g in $(find /sys/kernel/iommu_groups/* -maxdepth 0 -type d | sort -V); do
+    echo "IOMMU Group ${g##*/}:"
+    for d in $g/devices/*; do
+        echo -e "\t$(lspci -nns ${d##*/})"
+    done;
+done;
