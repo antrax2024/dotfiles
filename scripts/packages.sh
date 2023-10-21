e1bcbb84b427da8ea56661374834a5724d2b24b4
gargulaonline <fredcox@gmail.com>
2023-10-21 12:11:47 -0300

    ok

--- scripts/packages.sh
+++ scripts/packages.sh
@@ -1,115 +1,117 @@
 #!/usr/bin/bash
 # Pacotes pacman
 PACMAN_PKGS=(
-cargo
-mpd
-wget
-rsync
-sshfs
-exa
-bat
-ethtool
-evince
-cronie
-gthumb
-gimp
-tumbler
-yt-dlp
-lm_sensors
-w3m
-fish
-python-pip
-greetd
-grub-customizer
-xorg-xhost
-breeze
-cliphist
-xarchiver
-unzip
-system-config-printer
-usbutils
-avahi
-nss-mdns
-nvme-cli
-networkmanager
-network-manager-applet
-dialog
-mtools
-dosfstools
-snapper
-bluez
-bluez-utils
-xdg-utils
-xdg-user-dirs
-alsa-utils
-inetutils
-terminus-font
-nmap
-less
-blueman
-pipewire-jack
-pipewire-alsa
-pipewire-pulse
-limine
-speedcrunch
-discord
-telegram-desktop
-imagemagick
-ntp
-pavucontrol
-mlocate
-copyq
-python-psutil
-vulkan-tools
-powerline
-powerline-fonts
-foot
-xterm
-gtkmm
+    nodejs
+    npm
+    cargo
+    mpd
+    wget
+    rsync
+    sshfs
+    exa
+    bat
+    ethtool
+    evince
+    cronie
+    gthumb
+    gimp
+    tumbler
+    yt-dlp
+    lm_sensors
+    w3m
+    fish
+    python-pip
+    greetd
+    grub-customizer
+    xorg-xhost
+    breeze
+    cliphist
+    xarchiver
+    unzip
+    system-config-printer
+    usbutils
+    avahi
+    nss-mdns
+    nvme-cli
+    networkmanager
+    network-manager-applet
+    dialog
+    mtools
+    dosfstools
+    snapper
+    bluez
+    bluez-utils
+    xdg-utils
+    xdg-user-dirs
+    alsa-utils
+    inetutils
+    terminus-font
+    nmap
+    less
+    blueman
+    pipewire-jack
+    pipewire-alsa
+    pipewire-pulse
+    limine
+    speedcrunch
+    discord
+    telegram-desktop
+    imagemagick
+    ntp
+    pavucontrol
+    mlocate
+    copyq
+    python-psutil
+    vulkan-tools
+    powerline
+    powerline-fonts
+    foot
+    xterm
+    gtkmm
 )
 
 # pacotes AUR
 AUR_PKGS=(
-waybar-module-pacman-updates-git
-swaync
-cifs-utils
-fuzzel
-waybar-hyprland
-hyprland-git
-papirus-icon-theme
-thunar
-thunar-archive-plugin
-thunar-media-tags-plugin
-thunar-volman
-polkit-kde-agent
-qt5-wayland
-qt6-wayland
-nwg-look
-insync
-ttf-hack-nerd
-siji-ng
-chaotic-aur/wpaperd
-hyprshot
-ttf-firacode-nerd
-ttf-firacode
-ttf-font-awesome
-swayidle
-wlogout
-visual-studio-code-bin
-awesome-terminal-fonts
-gdu
-firefox
-adapta-gtk-theme
-wol-systemd
-neofetch
-swaylock-effects
-starship
-noto-fonts-emoji
-gohugo-extended-bin
-ttf-iosevka-nerd
-python-pipx
-qmplay2-git
-unimatrix-git
-pasystray-wayland
-ttf-hack
+    waybar-module-pacman-updates-git
+    swaync
+    cifs-utils
+    fuzzel
+    waybar-hyprland
+    hyprland-git
+    papirus-icon-theme
+    thunar
+    thunar-archive-plugin
+    thunar-media-tags-plugin
+    thunar-volman
+    polkit-kde-agent
+    qt5-wayland
+    qt6-wayland
+    nwg-look
+    insync
+    ttf-hack-nerd
+    siji-ng
+    chaotic-aur/wpaperd
+    hyprshot
+    ttf-firacode-nerd
+    ttf-firacode
+    ttf-font-awesome
+    swayidle
+    wlogout
+    visual-studio-code-bin
+    awesome-terminal-fonts
+    gdu
+    firefox
+    adapta-gtk-theme
+    wol-systemd
+    neofetch
+    swaylock-effects
+    starship
+    noto-fonts-emoji
+    gohugo-extended-bin
+    ttf-iosevka-nerd
+    python-pipx
+    qmplay2-git
+    unimatrix-git
+    pasystray-wayland
+    ttf-hack
 )
