a96ef02c6c5795a51719ce9be69c0561116f6e0c
gargulaonline <fredcox@gmail.com>
2023-10-21 22:04:55 -0300

    tabstop

--- nvim/options.lua
+++ nvim/options.lua
@@ -16,15 +16,21 @@ return {
     diagnostics_mode = 3, -- set the visibility of diagnostics in the UI (0=off, 1=only show in status line, 2=virtual text off, 3=all on)
     icons_enabled = true, -- disable icons in the UI (disable if no nerd font is available, requires :PackerSync after changing)
     ui_notifications_enabled = true, -- disable notifications when toggling UI elements
+    
     resession_enabled = false, -- enable experimental resession.nvim session management (will be default in AstroNvim v4)
+	  tabstop = 4,
+  	shiftwidth = 4,
+
   },
 }
 -- If you need more control, you can use the function()...end notation
--- return function(local_vim)
---   local_vim.opt.relativenumber = true
---   local_vim.g.mapleader = " "
---   local_vim.opt.whichwrap = vim.opt.whichwrap - { 'b', 's' } -- removing option from list
---   local_vim.opt.shortmess = vim.opt.shortmess + { I = true } -- add to option list
---
---   return local_vim
--- end
+--return function(local_vim)
+--local_vim.opt.relativenumber = true
+--local_vim.g.mapleader = " "
+--local_vim.opt.whichwrap = vim.opt.whichwrap - { 'b', 's' } -- removing option from list
+--local_vim.opt.shortmess = vim.opt.shortmess + { I = true } -- add to option list
+--return local_vim
+--end
+  
+
+
--- nvim/plugins/community.lua
+++ nvim/plugins/community.lua
@@ -2,7 +2,6 @@ return {
   -- Add the community repository of plugin specifications
   "AstroNvim/astrocommunity",
   { import = "astrocommunity.editing-support.rainbow-delimiters-nvim" },
-  { import = "astrocommunity.indent.indent-blankline-nvim" },
   { import = "astrocommunity.markdown-and-latex.glow-nvim" },
   { import = "astrocommunity.color.ccc-nvim" },
   { import = "astrocommunity.lsp.lsp-signature-nvim" },
