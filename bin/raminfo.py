d37b49f61e32a955e0ddda05b8281c691c35e7b3
gargulaonline <fredcox@gmail.com>
2023-10-24 19:11:05 -0300

    ok

---
+++ bin/raminfo.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python 
+
+import psutil
+
+factor = (1024 * 1024 * 1024)
+
+used=(psutil.virtual_memory().used)/ factor
+total=psutil.virtual_memory().total/ factor
+
+
+print("{}G / {} G".format(round(used,2),round(total,3)))
+
