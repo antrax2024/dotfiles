32a3a3d016053b5512dca62fbd8e7f74dcb518a5
gargulaonline <fredcox@gmail.com>
2023-10-24 19:28:36 -0300

    ok

---
+++ bin/gettime.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python 
+
+import datetime
+
+#dt_string = now.strftime("%d/%m/%Y %H:%M:%S")
+#print(dt_string)
+current_time = datetime.datetime.now()
+print(current_time.strftime('%H:%M:%S'))
