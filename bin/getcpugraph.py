b94118c36ab90316c8ed200a469c91fb8e2a6066
gargulaonline <fredcox@gmail.com>
2023-10-25 06:03:51 -0300

    ok

--- bin/getcpugraph.py
+++ bin/getcpugraph.py
@@ -2,9 +2,8 @@
 
 import psutil
 
-cpuPercent = round(psutil.cpu_percent(interval=1, percpu=False))
+cpuPercent = psutil.cpu_percent(interval=1, percpu=False)
 
-graph = cpuPercent 
 
 print("{}".format(cpuPercent))
 
