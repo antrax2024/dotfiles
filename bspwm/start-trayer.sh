3357445584b467a5b83eaf45ff6362c3cf3343bc
gargulaonline <fredcox@gmail.com>
2023-10-25 08:44:09 -0300

    ok

--- bspwm/start-trayer.sh
+++ bspwm/start-trayer.sh
@@ -11,5 +11,7 @@ then
     killall trayer
 else 
     echo "not running"
-fi    
-trayer --edge top --align right --transparent true --tint 0x00000000 --SetDockType true  --alpha 200 --height 30 --distance 0 --margin 10 --widthtype request --SetPartialStrut true --expand true &
+fi
+
+#trayer --edge top --align right --transparent true --tint 0x00000000 --SetDockType true  --alpha 200 --height 30 --distance 0 --margin 10 --widthtype request --SetPartialStrut true --expand true &
+trayer --edge top --align right --transparent true --tint 0x000000 --SetDockType false --height 30 --distance 0 --margin 10 --widthtype request --SetPartialStrut true --expand true &
