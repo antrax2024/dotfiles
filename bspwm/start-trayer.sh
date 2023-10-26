dc778f0b1744b78536b7fa88c38d03f725845e53
gargulaonline <fredcox@gmail.com>
2023-10-26 06:24:58 -0300

    trayer

--- bspwm/start-trayer.sh
+++ bspwm/start-trayer.sh
@@ -14,4 +14,4 @@ else
 fi
 
 #trayer --edge top --align right --transparent true --tint 0x00000000 --SetDockType true  --alpha 200 --height 30 --distance 0 --margin 10 --widthtype request --SetPartialStrut true --expand true &
-trayer --edge top --align right --transparent true --tint 0x000000 --SetDockType false --height 30 --distance 0 --margin 10 --widthtype request --SetPartialStrut true --expand true &
+trayer --edge top --align right --transparent true --tint 0x000000 --SetDockType true --height 30 --distance 0 --margin 10 --widthtype request --SetPartialStrut true --expand true &
