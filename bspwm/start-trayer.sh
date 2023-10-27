21db54dec877d891959cb588d6b168dda5a12c3a
gargulaonline <fredcox@gmail.com>
2023-10-27 19:39:43 -0300

    its ok now hummmmm OHHH YES!!! YES Man!!! Oww Yess!

--- bspwm/start-trayer.sh
+++ bspwm/start-trayer.sh
@@ -13,5 +13,4 @@ else
     echo "not running"
 fi
 
-#trayer --edge top --align right --transparent true --tint 0x00000000 --SetDockType true  --alpha 200 --height 30 --distance 0 --margin 10 --widthtype request --SetPartialStrut true --expand true &
-trayer --edge top --align right --transparent true --tint 0x000000 --SetDockType false --height 30 --distance 0 --margin 10 --widthtype request --SetPartialStrut true --expand true &
+trayer --edge top --align right --transparent true --alpha 0 --tint 0x000000 --SetDockType true --height 30 --distance 0 --margin 10 --widthtype request --SetPartialStrut false --expand true --monitor DP-0 &
