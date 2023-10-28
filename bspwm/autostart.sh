dbfbded6cd6dadd6dd778d269c64a92136ed5d81
gargulaonline <fredcox@gmail.com>
2023-10-28 15:53:26 -0300

    ok

--- bspwm/autostart.sh
+++ bspwm/autostart.sh
@@ -8,31 +8,30 @@ fork_once setxkbmap br
 fork_once xsetroot -cursor_name left_ptr 
 
 # Auto start apps
-fork_once numlockx &
-fork_once picom --daemon &
-fork_once /usr/lib/polkit-kde-authentication-agent-1  &
-fork_once dunst & 
-fork_once flameshot &
+fork_once numlockx 
+fork_once picom --daemon 
+fork_once /usr/lib/polkit-kde-authentication-agent-1  
 
 # eww bar
 fork_once eww open bar 
+
+
+sleep 5
+# Tray Icons
+fork_once trayer --edge top --align right --transparent true --alpha 0 --tint 0x000000 --SetDockType true --height 30 --distance 0 --margin 10 --widthtype request --SetPartialStrut false --expand true --monitor DP-1 
+
+# tray Applets 
+fork_once insync start 
+fork_once blueman-applet 
+fork_once nm-applet 
+fork_once volctl
+fork_once copyq --start-server
 # Wallpaper
 fork_once $HOME/dotfiles/bspwm/change-wallpaper.sh 
 # change wallpaper after 30min
 fork_once xidle -timeout 1800 -program "$HOME/dotfiles/bspwm/change-wallpaper.sh" 
 # Screensaver
 fork_once xidle -timeout 120 -program "$HOME/dotfiles/bin/screensaver.sh" 
-
-
-# tray Applets 
-fork_once insync start &
-fork_once blueman-applet & 
-fork_once nm-applet &
-fork_once volctl &
-
-
-sleep 5
-# Tray Icons
-killall trayer
-trayer --edge top --align right --transparent true --alpha 0 --tint 0x000000 --SetDockType true --height 30 --distance 0 --margin 10 --widthtype request --SetPartialStrut false --expand true --monitor DP-1 
+fork_once dunst  
+fork_once flameshot 
 
