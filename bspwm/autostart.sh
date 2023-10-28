d40f6f98b97ae9aa84548f0e98c42f0bd39e4945
gargulaonline <fredcox@gmail.com>
2023-10-28 15:35:56 -0300

    ok

---
+++ bspwm/autostart.sh
@@ -0,0 +1,38 @@
+#!/usr/bin/bash
+##################################################
+# Prevent duplicate
+fork_once() { [ $(pidof -sx $1) ] || $@ & }
+##################################################
+
+fork_once setxkbmap br 
+fork_once xsetroot -cursor_name left_ptr 
+
+# Auto start apps
+fork_once numlockx &
+fork_once picom --daemon &
+fork_once /usr/lib/polkit-kde-authentication-agent-1  &
+fork_once dunst & 
+fork_once flameshot &
+
+# eww bar
+fork_once eww open bar 
+# Wallpaper
+fork_once $HOME/dotfiles/bspwm/change-wallpaper.sh 
+# change wallpaper after 30min
+fork_once xidle -timeout 1800 -program "$HOME/dotfiles/bspwm/change-wallpaper.sh" 
+# Screensaver
+fork_once xidle -timeout 120 -program "$HOME/dotfiles/bin/screensaver.sh" 
+
+
+# tray Applets 
+fork_once insync start &
+fork_once blueman-applet & 
+fork_once nm-applet &
+fork_once volctl &
+
+
+sleep 5
+# Tray Icons
+killall trayer
+trayer --edge top --align right --transparent true --alpha 0 --tint 0x000000 --SetDockType true --height 30 --distance 0 --margin 10 --widthtype request --SetPartialStrut false --expand true --monitor DP-1 
+
