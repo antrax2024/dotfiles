4bf39388cd79dd63e3ce5b5c7c04658b372c7f48
gargulaonline <fredcox@gmail.com>
2023-11-04 13:40:55 -0300

    ok

--- bspwm/autostart.sh
+++ bspwm/autostart.sh
@@ -8,8 +8,8 @@ function run {
 }
 
 #Find out your monitor name with xrandr or arandr (save and you get this line)
-#xrandr --output VGA-1 --primary --mode 1360x768 --pos 0x0 --rotate normal
-#xrandr --output DP2 --primary --mode 1920x1080 --rate 60.00 --output LVDS1 --off &
+# Monitor em DP-0
+xrandr --output DP-0 --primary --mode 2560x1080 --rate 144.00 &
 #xrandr --output LVDS1 --mode 1366x768 --output DP3 --mode 1920x1080 --right-of LVDS1
 #xrandr --output HDMI2 --mode 1920x1080 --pos 1920x0 --rotate normal --output HDMI1 --primary --mode 1920x1080 --pos 0x0 --rotate normal --output VIRTUAL1 --off
 #autorandr horizontal
