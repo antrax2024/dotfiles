8bd1b8d7c3b5bcf670907e9783cdd267afbcaed5
gargulaonline <fredcox@gmail.com>
2023-11-08 22:07:55 -0300

    ok

--- artix-env/scripts/kvm-qemu-install.sh
+++ artix-env/scripts/kvm-qemu-install.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/bash
 
-#sudo pacman -Syy
-#sudo pacman -S qemu-full virt-manager dnsmasq vde2 \
-#bridge-utils openbsd-netcat dmidecode ebtables iptables libguestfs \
-#edk2-ovmf swtpm libvirt-runit
+sudo pacman -Syy
+sudo pacman -S qemu-full virt-manager dnsmasq vde2 \
+bridge-utils openbsd-netcat dmidecode ebtables iptables libguestfs \
+edk2-ovmf swtpm libvirt-runit
 
 
 sudo nvim /etc/libvirt/libvirtd.conf
@@ -12,6 +12,10 @@ sudo usermod -aG libvirt $USER
 
 sudo ln -s /etc/runit/sv/libvirtd /run/runit/service/
 sudo sv up libvirtd
+sudo ln -s /etc/runit/sv/virtlogd/ /run/runit/service/
+sudo sv up virtlogd
+sudo ln -s /etc/runit/sv/virtlockd/ /run/runit/service/
+sudo sv up virtlockd
 
 sudo virsh net-start default
 sudo virsh net-autostart default
