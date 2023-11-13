42dc9befa7a5fe971e277b9542e2248d2567dee1
gargulaonline <fredcox@gmail.com>
2023-11-13 06:51:50 -0300

    ok

--- arco-env/vm-install-gnome.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/bash
-
-paru
-
-sudo systemctl disable greetd.service
-paru -S gnome gnome-tweaks gnome-extra gdm spice-vdagent gnome-browser-connector
-sudo systemctl enable gdm.service
-
-
