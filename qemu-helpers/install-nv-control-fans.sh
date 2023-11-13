3b32ad46420fcc6e951c3cd1f0244a970039d50e
gargulaonline <fredcox@gmail.com>
2023-11-13 11:29:03 -0300

    ok

---
+++ qemu-helpers/install-nv-control-fans.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/bash
+#
+sudo cp ~/dotfiles/qemu-helpers/nvidia-fan-control/* /usr/bin/
+
+echo 'OK!'
---
+++ qemu-helpers/install-sunshine.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/bash
+#
+sudo pacman -S wget
+
+mkdir ~/sunshine/
+
+cd ~/sunshine/
+
+wget https://github.com/LizardByte/Sunshine/releases/download/v0.21.0/sunshine.AppImage
+
+
