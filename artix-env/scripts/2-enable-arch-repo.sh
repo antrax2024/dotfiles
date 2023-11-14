c2719ae014ab5d1a6e3368336022e237ef234a8e
gargulaonline <fredcox@gmail.com>
2023-11-14 10:02:28 -0300

    ok

--- artix-env/scripts/2-enable-arch-repo.sh
+++ artix-env/scripts/2-enable-arch-repo.sh
@@ -3,10 +3,13 @@
 source /home/gargula/dotfiles/bin/functions.sh
 
 TARGET='/etc/pacman.conf'
+APPEND_CMD='sudo sed -i'
 
 
 printStatus "Performing upgrading...."
 sudo pacman -Syu
+
+
 
 printStatus "Enable artix-archlinux-support..."
 sudo pacman -Syu artix-archlinux-support
@@ -14,17 +17,17 @@ sudo pacman -Syu artix-archlinux-support
 printStatus "Editing /etc/pacman.conf..."
 
 # Arch repositories
-sudo sed -i -e '############################################################' $TARGET
-sudo sed -i -e '#Arch repositories' $TARGET
-sudo sed -i -e '[extra]' $TARGET
-sudo sed -i -e 'Include = /etc/pacman.d/mirrorlist-arch' $TARGET
-
-sudo sed -i -e '[community]' $TARGET
-sudo sed -i -e 'Include = /etc/pacman.d/mirrorlist-arch' $TARGET
-
-sudo sed -i -e '[multilib]' $TARGET
-sudo sed -i -e 'Include = /etc/pacman.d/mirrorlist-arch' $TARGET
-sudo sed -i -e '############################################################' $TARGET
+$APPEND_CMD '############################################################' $TARGET
+$APPEND_CMD '#Arch repositories' $TARGET
+$APPEND_CMD '[extra]' $TARGET
+$APPEND_CMD 'Include = /etc/pacman.d/mirrorlist-arch' $TARGET
+$APPEND_CMD '#' $TARGET
+$APPEND_CMD '[community]' $TARGET
+$APPEND_CMD 'Include = /etc/pacman.d/mirrorlist-arch' $TARGET
+$APPEND_CMD '#' $TARGET
+$APPEND_CMD '[multilib]' $TARGET
+$APPEND_CMD 'Include = /etc/pacman.d/mirrorlist-arch' $TARGET
+sudo sed -i '############################################################' $TARGET
 
 
 printStatus "Populating...."
