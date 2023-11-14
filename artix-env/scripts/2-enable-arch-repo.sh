e4f3075bd5c9180d250e2c47aa0affbca91da580
gargulaonline <fredcox@gmail.com>
2023-11-14 10:13:19 -0300

    ok

--- artix-env/scripts/2-enable-arch-repo.sh
+++ artix-env/scripts/2-enable-arch-repo.sh
@@ -3,39 +3,24 @@
 source /home/gargula/dotfiles/bin/functions.sh
 
 TARGET='/etc/pacman.conf'
-APPEND_CMD='sudo sed -i'
 
+printStatus "Editing $TARGET"
+sudo nvim $TARGET
 
 printStatus "Performing upgrading...."
 sudo pacman -Syu
-
-
 
 printStatus "Enable artix-archlinux-support..."
 sudo pacman -Syu artix-archlinux-support
 
 printStatus "Editing /etc/pacman.conf..."
-
-# Arch repositories
-$APPEND_CMD '############################################################' $TARGET
-$APPEND_CMD '#Arch repositories' $TARGET
-$APPEND_CMD '[extra]' $TARGET
-$APPEND_CMD 'Include = /etc/pacman.d/mirrorlist-arch' $TARGET
-$APPEND_CMD '#' $TARGET
-$APPEND_CMD '[community]' $TARGET
-$APPEND_CMD 'Include = /etc/pacman.d/mirrorlist-arch' $TARGET
-$APPEND_CMD '#' $TARGET
-$APPEND_CMD '[multilib]' $TARGET
-$APPEND_CMD 'Include = /etc/pacman.d/mirrorlist-arch' $TARGET
-$APPEND_CMD '############################################################' $TARGET
+sudo cat ./arch-repos.conf >> $TARGET
 
 
 printStatus "Populating...."
 sudo pacman-key --populate archlinux
 
 sudo pacman -Syu
-
-
 
 printStatus "Thats all..."
 
