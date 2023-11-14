3dfa79c75300b892783e9d5fe91c3ac2db65ac53
gargulaonline <fredcox@gmail.com>
2023-11-14 09:16:03 -0300

    ok

--- artix-env/scripts/2-enable-arch-repo.sh
+++ artix-env/scripts/2-enable-arch-repo.sh
@@ -1,5 +1,17 @@
 #!/usr/bin/bash
 #
+source /home/gargula/dotfiles/bin/functions.sh
+
+
+printStatus "Performing upgrading...."
+sudo pacman -Syu
+
+printStatus "Enable artix-archlinux-support..."
 sudo pacman -Syu artix-archlinux-support
+
+
+
+
+
 
 
