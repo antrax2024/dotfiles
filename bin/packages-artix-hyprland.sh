d98e4df5b550379044d969b44decf00011562cea
gargulaonline <fredcox@gmail.com>
2023-11-15 18:32:47 -0300

    ok

---
+++ bin/packages-artix-hyprland.sh
@@ -0,0 +1,100 @@
+#!/usr/bin/bash
+# Pacotes para hyprland
+PKGS=(
+	#########################
+	# APPS
+	#########################
+	xdg-user-dirs
+	firefox
+	feh
+	less
+	python-pynvim
+	python-pyserial
+	nodejs
+	npm
+	extra/eza
+	gimp
+	world/pacman-contrib
+	extra/parallel
+	yt-dlp
+	extra/nmap
+	extra/bat
+	python-psutil
+	visual-studio-code-bin
+	extra/gdu
+	extra/duf
+	extra/starship
+	alacritty-git
+	aur/compfy
+	extra/dunst
+	extra/flameshot
+	openssh
+	openssh-runit
+	mesa-vdpau
+	libva-mesa-driver
+	chaotic-aur/moonlight-qt
+	obs-studio
+	neofetch
+	fish
+	pipewire
+	pipewire-pulse
+	pipewire-alsa
+	pipewire-jack
+	wireplumber
+	bluez
+	bluez-utils
+	bluez-runit
+	blueberry
+	thunar-extended
+	thunar-archive-plugin
+	thunar-media-tags-plugin
+	thunar-volman
+	chaotic-aur/insync
+	file-roller
+	tumbler
+	cmake
+	mlocate
+	extra/lazygit
+	ripgrep
+	aur/bottom-nightly-bin
+	telegram-desktop
+	pavucontrol
+	wget
+	cronie
+	cronie-runit
+	ntp
+	ntp-runit
+	#########################
+	# Themes
+	chaotic-aur/nwg-look-bin
+	adwaita-cursors
+	adwaita-icon-theme
+	adw-gtk-theme
+	adwaita-dark-darose
+	papirus-icon-theme
+	breeze
+	extra/arc-gtk-theme
+	#########################
+	# FONTS 
+	#########################
+	ttf-jetbrains-mono-nerd
+	ttc-iosevka
+	ttf-nerd-fonts-symbols
+	ttf-icomoon-feather
+	ttf-firacode-nerd
+	ttf-hack-nerd
+	ttf-font-awesome
+	awesome-terminal-fonts
+	noto-fonts-emoji
+	chaotic-aur/ttf-material-design-icons-extended
+	#########################
+	)
+
+LINK_DIRS=(
+	alacritty
+	dunst
+	fish
+	neofetch
+)
+
+
