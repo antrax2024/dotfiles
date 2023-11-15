ab0b827bc1a03f4308b10ff16a57e503cf7469a1
gargulaonline <fredcox@gmail.com>
2023-11-14 21:29:04 -0300

    ok

--- artix-env/scripts/4-kvm-qemu-install.sh
+++ artix-env/scripts/4-kvm-qemu-install.sh
@@ -27,14 +27,16 @@ paru -S ${PKGS[*]}
 sudo nvim /etc/libvirt/libvirtd.conf
 
 sudo usermod -aG libvirt $USER
-
 sudo ln -s /etc/runit/sv/libvirtd /run/runit/service/
-sudo sv up libvirtd
 sudo ln -s /etc/runit/sv/virtlogd/ /run/runit/service/
-sudo sv up virtlogd
 sudo ln -s /etc/runit/sv/virtlockd/ /run/runit/service/
+
+
+sudo sv up libvirtd
+sudo sv up virtlogd
 sudo sv up virtlockd
 
+sleep 3
 sudo virsh net-start default
 sudo virsh net-autostart default
 sudo virsh net-list --all
