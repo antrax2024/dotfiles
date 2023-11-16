7c069f8151a61ff9d350728970e23be664bce3b8
gargulaonline <fredcox@gmail.com>
2023-11-16 10:23:09 -0300

    ok

---
+++ artix-env/scripts/install-nvidia.sh
@@ -0,0 +1,90 @@
+# CONFIG VARS
+#
+USERNAME='gargula'
+EDITOR='nvim'
+NET_IFACE='eth0'
+DOTFILES="$HOME/dotfiles"
+SERVICE='/run/runit/service'
+
+source ../../bin/functions.sh
+source ../../bin/packages-artix.sh
+
+printStatus "Upgrading..."
+sudo pacman -Syu
+
+printStatus "Update grub..."
+sudo ln -sf $DOTFILES/artix-env/etc/default/grub /etc/default/grub
+sudo grub-mkconfig -o /boot/grub/grub.cfg
+
+printStatus "Linking mkinitcpio.conf"
+sudo ln -sf $DOTFILES/artix-env/etc/mkinitcpio.conf /etc/mkinitcpio.conf
+
+printStatus "Install paru..."
+git clone https://aur.archlinux.org/paru.git
+cd paru
+makepkg -si
+cd ../
+rm -rfv paru
+
+printStatus "Enable Chaotic-AUR..."
+sudo pacman-key --recv-key 3056513887B78AEB --keyserver keyserver.ubuntu.com
+sudo pacman-key --lsign-key 3056513887B78AEB
+sudo pacman -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-keyring.pkg.tar.zst' 
+sudo pacman -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-mirrorlist.pkg.tar.zst' 
+
+sudo sed -i -e '$a[chaotic-aur]' /etc/pacman.conf
+sudo sed -i -e '$aInclude = /etc/pacman.d/chaotic-mirrorlist' /etc/pacman.conf
+
+paru
+
+printStatus "Install Packages"
+
+paru -S ${PKGS[*]}
+
+
+#printStatus "Enable Wake on LAN..."
+#sudo systemctl enable "wol@$NET_IFACE".service
+
+
+printStatus "Prepare to link..."
+# get length of an array
+arraylength=${#LINK_DIRS[@]}
+
+mkdir $HOME/.config
+
+# use for loop to read all values and indexes
+for (( i=0; i<${arraylength}; i++ ));
+do
+	printStatus "Linking ${LINK_DIRS[$i]}"
+	verifyIfConfigExists "${LINK_DIRS[$i]}"
+	ln -sf "$DOTFILES/${LINK_DIRS[$i]}" "$HOME/.config/${LINK_DIRS[$i]}"
+done
+
+printStatus "Changind default shell to Fish Shell..."
+chsh -s /usr/bin/fish $USERNAME
+
+printStatus "Enable openSSH..."
+sudo ln -s /etc/runit/sv/sshd $SERVICE
+
+printStatus "Enable bluetooth"
+sudo ln -s /etc/runit/sv/bluetoothd $SERVICE
+
+printStatus "Enable lightdm..."
+sudo cp -fv $DOTFILES/artix-env/etc/lightdm/lightdm.conf /etc/lightdm/
+
+printStatus "Enable cronie..."
+sudo ln -s /etc/runit/sv/cronie $SERVICE
+
+printStatus "Enable NTP Sync"
+sudo ln -s /etc/runit/sv/ntpd /run/runit/service
+
+printStatus "Enable Swap File... Hummmmmm"
+sudo fallocate -l 4G /swapfile
+sudo chmod 600 /swapfile
+sudo mkswap /swapfile
+sudo swapon /swapfile
+echo '# Swapfile' | sudo tee --append /etc/fstab
+echo '/swapfile none swap defaults 0 0' | sudo tee --append /etc/fstab
+
+
+printStatus "Thats all"
