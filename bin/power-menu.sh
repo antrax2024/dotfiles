cc00df7ff033db8d201c6800983f830dc9da73f6
gargulaonline <fredcox@gmail.com>
2023-11-17 21:24:10 -0300

    ok

--- bin/power-menu.sh
+++ bin/power-menu.sh
@@ -1,14 +1,10 @@
 #!/bin/bash
 
-entries="⇠ Logout\n⏾ Suspend\n⭮ Reboot\n⏻ Shutdown"
+entries="⭮ Reboot\n⏻ Shutdown"
 
-selected=$(echo -e $entries|wofi --width 250 --height 210 --dmenu --cache-file /dev/null | awk '{print tolower($2)}')
+selected=$(echo -e $entries|wofi --width 250 --height 110 --dmenu --cache-file /dev/null | awk '{print tolower($2)}')
 
 case $selected in
-  logout)
-    swaymsg exit;;
-  suspend)
-    exec loginctl suspend;;
   reboot)
     exec loginctl reboot;;
   shutdown)
