331b944fcb7ea304e583f26bbbdf592deae0d821
gargulaonline <fredcox@gmail.com>
2023-11-19 14:40:38 -0300

    ok

--- nvim/plugins/user.lua
+++ nvim/plugins/user.lua
@@ -9,6 +9,17 @@ return {
   --     require("lsp_signature").setup()
   --   end,
   -- },
+  {
+	  "L3MON4D3/LuaSnip",
+	  -- follow latest release.
+	  version = "v2.0.0",
+	  dependencies = { "rafamadriz/friendly-snippets" },
+	  -- install jsregexp (optional!).
+	  build = "make install_jsregexp",
+	  require("luasnip.loaders.from_vscode").load_standalone({path = "/home/gargula/dotfiles/nvim/snippets.json"})
+  },
+
+  { "elkowar/yuck.vim" },
 
 }
 
