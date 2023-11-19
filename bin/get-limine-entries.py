16f9af951bba4e11dff345545bd575220435c808
gargulaonline <fredcox@gmail.com>
2023-11-19 11:50:15 -0300

    ok

---
+++ bin/get-limine-entries.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python
+import glob
+
+kernels = glob.glob('/boot/vmlinuz*')
+print('Found {} kernels: '.format(len(kernels)))
+for k in kernels:
+    kernelLine = k.replace('/boot/vmlinuz-', '')
+    print(':{}'.format(kernelLine))
+    print('\tPROTOCOL=linux')
+    print('\tKERNEL_PATH=uuid://${}/boot/vmlinuz-{}'.format('ROOT_UUID',kernelLine))
+    print('\tCMDLINE=root=UUID=${ROOT_UUID} rw ${KERNEL_PARAMS}')
+    print('\tMODULE_PATH=uuid://${}/boot/initramfs-{}.img'.format('ROOT_UUID',kernelLine))
+
+
+
+
+
+
+
