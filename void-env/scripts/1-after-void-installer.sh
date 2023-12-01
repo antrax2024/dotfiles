b96e494b68819e9f2e42ce6b06cbaf1ed93bb939
gargulaonline <fredcox@gmail.com>
2023-12-01 10:21:15 -0300

    oik

--- void-env/scripts/1-after-void-installer.sh
+++ void-env/scripts/1-after-void-installer.sh
@@ -6,7 +6,7 @@ export EDITOR='nvim'; visudo
 
 echo "Install Limine bootloader..."
 
-xbps-install limine git neovim fish-shell
+xbps-install limine neovim fish-shell
 mkdir -p /boot/efi/EFI/BOOT
 cp /usr/share/limine/BOOTX64.EFI /boot/efi/EFI/BOOT
 cp ../boot/limine.cfg /boot/
