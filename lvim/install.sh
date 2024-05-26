63748ba30b624b8944c96eadf3261fccceef3204
gonha <fredcox@gmail.com>
2024-05-26 12:52:24 -0300

    ok

--- lvim/install.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/bash
-#
-
-
-LV_BRANCH='release-1.4/neovim-0.9' bash <(curl -s https://raw.githubusercontent.com/LunarVim/LunarVim/release-1.4/neovim-0.9/utils/installer/install.sh)
-
-rm ~/$HOME/.config/lvim/config.lua
-ln -sf $HOME/dotfiles/lvim/config.lua $HOME/.config/lvim/config.lua
