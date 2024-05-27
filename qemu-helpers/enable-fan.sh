8c8b21bc8a454985ae352e953ad94276ca55cce3
gargula <fredcox@gmail.com>
2024-05-27 11:46:49 -0300

    ok

--- qemu-helpers/enable-fan.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/bash
-sudo nvidia-xconfig -a --cool-bits=28 --allow-empty-initial-configuration
-
-nvidia-smi -i 0 -f 75
---
+++ qemu-helpers/enable-network.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/bash
+
+sudo virsh net-autostart default
+sudo virsh net-list --all
