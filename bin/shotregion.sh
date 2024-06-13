8f4b5ef09eb0e9e84aec6d299b6d2b18f85ef69b
gargula <fredcox@gmail.com>
2024-06-13 08:23:18 -0300

    ok

--- bin/shotregion.sh
+++ bin/shotregion.sh
@@ -3,7 +3,7 @@
 DIRECTORY="$HOME/Pictures"
 FILENAME=$(date +"%d%m%Y%H%M%S%N").png
 
-hyprshot -m output -o $DIRECTORY -f $FILENAME
+hyprshot -m region -o $DIRECTORY -f $FILENAME
 
 sleep 1
 swappy -f "$DIRECTORY/$FILENAME"
