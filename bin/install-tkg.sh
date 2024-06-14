cdb3e63c18b5e86284d74415af591169be99974b
gargula <fredcox@gmail.com>
2024-06-14 07:20:02 -0300

    ok

--- bin/install-tkg.sh
+++ bin/install-tkg.sh
@@ -2,4 +2,11 @@
 
 source rainbow.sh
 
+echoblue "*****************************"
+echoyellow "Cloning Repository..."
+echoblue "*****************************"
+git clone https://github.com/Frogging-Family/linux-tkg.git
+cd linux-tkg
+makepkg -si
+
 echogreen 'Finish...'
