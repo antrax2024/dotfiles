819f5180b26a9ad40ff889f865ce1a9cce9a1b77
antrax <fredcox@gmail.com>
2024-06-16 01:57:57 -0300

    ok

--- bin/install-chaotic-aur.sh
+++ bin/install-chaotic-aur.sh
@@ -1,14 +1,9 @@
 #!/bin/bash
 
-source "$HOME/dotfiles/bin/rainbow.sh"
-
 sudo pacman-key --recv-key 3056513887B78AEB --keyserver keyserver.ubuntu.com
 sudo pacman-key --lsign-key 3056513887B78AEB
 sudo pacman -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-keyring.pkg.tar.zst'
 sudo pacman -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-mirrorlist.pkg.tar.zst'
 
-
-echo "Append"
-echo "[chaotic-aur]"
-echo "Include = /etc/pacman.d/chaotic-mirrorlist"
-
+echo "[chaotic-aur]" | sudo tee -a /etc/pacman.conf >/dev/null
+echo "Include = /etc/pacman.d/chaotic-mirrorlist" | sudo tee -a /etc/pacman.conf >/dev/null
---
+++ bin/install-paru.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+git clone https://aur.archlinux.org/paru.git
+cd paru
+makepkg -si
+cd ..
+rm -rfv paru
