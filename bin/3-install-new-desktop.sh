cd667e890be2ed4844bb553ff56a460a4bc8a5c9
antrax <fredcox@gmail.com>
2024-06-16 07:48:59 -0300

    ok

--- bin/3-install-new-desktop.sh
+++ bin/3-install-new-desktop.sh
@@ -37,7 +37,7 @@ PACKAGES=(
 	neovim-git python-virtualenv lazygit python-pynvim noto-fonts-emoji fd npm nodejs python-pip python-virtualenv ripgrep
 	# Hyprland
 	hyprland hyprcursor hypridle hyprpaper hyprshot wl-clipboard xdg-desktop-portal-hyprland
-	wlogout swappy nwg-look sddm archlinux-themes-sddm numlockx
+	wlogout swappy nwg-look sddm-git archlinux-themes-sddm sddm-kcm
 )
 
 paru -S "${PACKAGES[@]}"
