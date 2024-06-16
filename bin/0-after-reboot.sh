1368d0dd4a5a74d43cc48d2071de2949207b1fc2
antrax <fredcox@gmail.com>
2024-06-16 11:12:30 -0300

    ok

---
+++ bin/0-after-reboot.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+source ./rainbow.sh
+
+echoyellow 'Adjust Grub...'
+
+sudo sed -i 's/loglevel=3 quiet/loglevel=3 nvidia_drm.modeset=1/g' /etc/default/grub
+sudo sed -i 's/#GRUB_DISABLE_OS_PROBER=false/GRUB_DISABLE_OS_PROBER=false/g' /etc/default/grub
