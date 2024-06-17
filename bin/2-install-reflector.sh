d65e1116fc485ebad0f9e360e1c5342db6dd48a9
antrax <fredcox@gmail.com>
2024-06-17 08:17:14 -0300

    ok

---
+++ bin/2-install-reflector.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+gA=$(echogreen "==>")
+
+echo "$gA Updating the System..."
+
+sudo pacman -Syyu
+
+echo "$gA Install Reflector..."
+sudo pacman -S reflector
+
+echo "$gA Edit /etc/xdg/reflector/reflector.conf..."
+sudo sed -i 's/# --country France,Germany/--country "United States",Chile/g' /etc/xdg/reflector/reflector.conf
+
+echo "$gA Enable and Start reflector.service..."
+sudo systemctl enable --now reflector.service
