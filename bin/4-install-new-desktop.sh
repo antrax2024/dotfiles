c3cd10f6b4059c7ec0923911631c26c6434dcf53
antrax <fredcox@gmail.com>
2024-06-17 19:00:02 -0300

    ok

--- bin/4-install-new-desktop.sh
+++ bin/4-install-new-desktop.sh
@@ -10,12 +10,9 @@ PACKAGES=(
 	# Utils
 	firefox-developer-edition surfshark-client keepassxc qbittorrent
 	onedrivegui zapzap 64gram-desktop mpv nvidia-settings okular yt-dlp
+	qmplay2-git
 	# System
-	cronie
-	cups
-	xorg-xhost
-	ntfs-3g
-	dmidecode
+	cronie cups xorg-xhost ntfs-3g dmidecode
 	# Thunar
 	thunar thunar-archive-plugin thunar-media-tags-plugin thunar-volman
 	plocate tumbler file-roller
