76fead8d7ff631152874a65427e5591f5c9501b7
antrax <fredcox@gmail.com>
2024-06-18 21:26:11 -0300

    ok

--- bin/dotupdate.sh
+++ bin/dotupdate.sh
@@ -5,7 +5,7 @@ source "/home/antrax/dotfiles/bin/rainbow.sh"
 
 ga=$(echogreen "==>")
 
-echo "Update $HOME/dotfiles..."
+echo "$ga Update $HOME/dotfiles..."
 cd "$HOME/dotfiles" || exit
 git add -A
 git commit -a
