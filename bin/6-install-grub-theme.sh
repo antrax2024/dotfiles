71a68702e664b4b04a4acd5d647550269f2a592d
antrax <fredcox@gmail.com>
2024-06-20 07:23:02 -0300

    ok

--- bin/6-install-grub-theme.sh
+++ bin/6-install-grub-theme.sh
@@ -10,6 +10,11 @@ git clone --depth 1 https://gitlab.com/VandalByte/darkmatter-grub-theme.git
 cd darkmatter-grub-theme || 1
 sudo python3 darkmatter-theme.py --install
 
+echo "$gA Editing /etc/default/grub..."
+
+sudo sed -i 's/GRUB_GFXMODE=auto/GRUB_GFXMODE=2560x1080/g' /etc/default/grub
+sudo grub-mkconfig -o /boo/grub/grub.cfg
+
 rm -rfv darkmatter-grub-theme
 
 echo "$gA Finish..."
