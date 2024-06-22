9c764a8ce0f30899e4327e013bc3523d4e55b100
antrax <fredcox@gmail.com>
2024-06-22 08:30:55 -0300

    ok

--- bin/4-install-new-desktop.sh
+++ bin/4-install-new-desktop.sh
@@ -1,40 +1,11 @@
 #!/bin/bash
 
 source "./rainbow.sh"
+source "./packages.sh"
 
 gA=$(echogreen "==>")
 
 #Install Packages
-PACKAGES=(
-	# Utils
-	firefox surfshark-client keepassxc qbittorrent
-	onedrivegui zapzap 64gram-desktop mpv nvidia-settings okular yt-dlp
-	qmplay2-git btop lsb-release qalculate-gtk
-	# System
-	cronie cups xorg-xhost ntfs-3g dmidecode system-config-printer hplip
-	# Thunar
-	thunar thunar-archive-plugin thunar-media-tags-plugin thunar-volman
-	plocate tumbler file-roller
-	# pipewire
-	pipewire pipewire-audio pipewire-pulse wireplumber pavucontrol jamesdsp
-	# bluetooth
-	bluez blueman bluez-utils
-	# neovim-git
-	neovim-git python-virtualenv lazygit python-pynvim noto-fonts-emoji fd npm nodejs python-pip python-virtualenv ripgrep
-	# Hyprland
-	hyprland hyprcursor hypridle hyprpaper hyprshot wl-clipboard xdg-desktop-portal-hyprland
-	wlogout swappy nwg-look sddm-git archlinux-themes-sddm sddm-kcm
-	alacritty-git neofetch-git copyq dunst fuzzel waybar-git
-	# Shell
-	eza duf gdu bat fish-git ttf-hack-nerd reflector starship
-	# Nvidia
-	nvidia-dkms
-	#xfce
-	xfce4 xfce4-goodies
-	# Themes
-	arc-gtk-theme papirus-icon-theme breeze
-)
-
 echo "$gA Install Packages..."
 
 paru -S "${PACKAGES[@]}"
---
+++ bin/packages.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+PACKAGES=(
+	# Utils
+	firefox surfshark-client keepassxc qbittorrent
+	onedrivegui zapzap 64gram-desktop mpv nvidia-settings okular yt-dlp
+	qmplay2-git btop lsb-release qalculate-gtk lximage-qt
+	# System
+	cronie cups xorg-xhost ntfs-3g dmidecode system-config-printer hplip
+	# Thunar
+	thunar thunar-archive-plugin thunar-media-tags-plugin thunar-volman
+	plocate tumbler file-roller
+	# pipewire
+	pipewire pipewire-audio pipewire-pulse wireplumber pavucontrol jamesdsp
+	# bluetooth
+	bluez blueman bluez-utils
+	# neovim-git
+	neovim-git python-virtualenv lazygit python-pynvim noto-fonts-emoji fd npm nodejs python-pip python-virtualenv ripgrep
+	# Hyprland
+	hyprland hyprcursor hypridle hyprpaper hyprshot wl-clipboard xdg-desktop-portal-hyprland
+	wlogout swappy nwg-look sddm-git archlinux-themes-sddm sddm-kcm
+	alacritty-git neofetch-git copyq dunst fuzzel waybar-git
+	# Shell
+	eza duf gdu bat fish-git ttf-hack-nerd reflector starship
+	# Nvidia
+	nvidia-dkms
+	#xfce
+	xfce4 xfce4-goodies
+	# Themes
+	arc-gtk-theme papirus-icon-theme breeze
+)
